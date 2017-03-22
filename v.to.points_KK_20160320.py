67fb4f74ae4211ad0a51fea2ee37cbd5a7eefd10
Joseph Kaelin <jjk@jkaelin.ch>
2017-03-22 16:52:06 +0000

    TunnelVariables.csv

--- v.to.points_KK_20160320.py
+++ v.to.points_KK_20160320.py
@@ -33,71 +33,70 @@ OstrohrR1_Felsoberflaeche = "/home/kaelin_joseph/TunnelGIS.Rheintunnel/OstrohrR1
 #                  OstrohrR1_csv,"NA",",",500, True,False,False,False,False,"2603510.0,2624270.0,1260650.0,1274890.0",
 #                  -1,0.0001,OstrohrR1_Felsoberflaeche)
 
-
 # Join results using Panda
 import pandas as pd
 
 OstrohrR1_csv = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/2017-03-19.OstrohrR1.csv")
-OstrohrR1_Felstiefe_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/OstrohrR1_Felstiefe.csv")
-OstrohrR1_DTM_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/OstrohrR1_DTM.csv")
-OstrohrR1_Felsoberflaeche_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/OstrohrR1_Felsoberflaeche.csv")
+OstrohrR1_Felstiefe_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/OstrohrR1_Felstiefe.csv")
+OstrohrR1_DTM_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/OstrohrR1_DTM.csv")
+OstrohrR1_Felsoberflaeche_csv  = pd.read_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/OstrohrR1_Felsoberflaeche.csv")
 
 OstrohrR1_csv.head()
-OstrohrR1_csv.columns
 OstrohrR1_csv = OstrohrR1_csv.loc[:,["Xcoord", "Ycoord", "Zcoord"]]
 OstrohrR1_csv.head()
 
 OstrohrR1_Felstiefe_csv.head()
-OstrohrR1_Felstiefe_csv.columns
-OstrohrR1_Felstiefe_csv = OstrohrR1_Felstiefe_csv.loc[:,["easting", "northing", "tmp14900475028728"]]
-OstrohrR1_Felstiefe_csv.head()
+OstrohrR1_Felstiefe_csv_coleqtmp = [col for col in OstrohrR1_Felstiefe_csv.columns if 'tmp' in col]
+if len(OstrohrR1_Felstiefe_csv_coleqtmp) != 1:
+    print "Rename of Felstiefe attribute did not work properly. Please check"
+    exit()
+OstrohrR1_Felstiefe_csv_rename = OstrohrR1_Felstiefe_csv.rename(columns= {OstrohrR1_Felstiefe_csv_coleqtmp[0]: "Felstiefe"})
+OstrohrR1_Felstiefe_csv_sel = OstrohrR1_Felstiefe_csv_rename.loc[:,["easting", "northing", "Felstiefe"]]
+OstrohrR1_Felstiefe_csv_sel.head()
 
 OstrohrR1_DTM_csv.head()
-OstrohrR1_DTM_csv.columns
-OstrohrR1_DTM_csv = OstrohrR1_DTM_csv.loc[:,["easting", "northing", "tmp1490047503330"]]
-OstrohrR1_DTM_csv.head()
+OstrohrR1_DTM_csv_coleqtmp = [col for col in OstrohrR1_DTM_csv.columns if 'tmp' in col]
+if len(OstrohrR1_DTM_csv_coleqtmp) != 1:
+    print "Rename of Felstiefe attribute did not work properly. Please check"
+    exit()
+OstrohrR1_DTM_csv_rename = OstrohrR1_DTM_csv.rename(columns= {OstrohrR1_DTM_csv_coleqtmp[0]: "DTM"})
+OstrohrR1_DTM_csv_sel = OstrohrR1_DTM_csv_rename.loc[:,["easting", "northing", "DTM"]]
+OstrohrR1_DTM_csv_sel.head()
 
 OstrohrR1_Felsoberflaeche_csv.head()
-OstrohrR1_Felsoberflaeche_csv.columns
-OstrohrR1_Felsoberflaeche_csv = OstrohrR1_Felsoberflaeche_csv.loc[:,["easting", "northing", "tmp14900475037932"]]
-OstrohrR1_Felsoberflaeche_csv.head()
+OstrohrR1_Felsoberflaeche_csv_coleqtmp = [col for col in OstrohrR1_Felsoberflaeche_csv.columns if 'tmp' in col]
+if len(OstrohrR1_Felsoberflaeche_csv_coleqtmp) != 1:
+    print "Rename of Felstiefe attribute did not work properly. Please check"
+    exit()
+OstrohrR1_Felsoberflaeche_csv_rename = OstrohrR1_Felsoberflaeche_csv.rename(columns= {OstrohrR1_Felsoberflaeche_csv_coleqtmp[0]: "Felsoberflache"})
+OstrohrR1_Felsoberflaeche_csv_sel = OstrohrR1_Felsoberflaeche_csv_rename.loc[:,["easting", "northing", "Felsoberflache"]]
+OstrohrR1_Felsoberflaeche_csv_sel.head()
 
-merge_felstiefe = pd.merge(OstrohrR1_csv, OstrohrR1_Felstiefe_csv, 
+merge_felstiefe = pd.merge(OstrohrR1_csv, OstrohrR1_Felstiefe_csv_sel, 
                  left_on = ["Xcoord","Ycoord"], 
                  right_on = ["easting","northing"])
 merge_felstiefe.head()
 merge_felstiefe.columns
                  
-merge_felstiefe_dtm = pd.merge(merge_felstiefe, OstrohrR1_DTM_csv, 
+merge_felstiefe_dtm = pd.merge(merge_felstiefe, OstrohrR1_DTM_csv_sel, 
                  left_on = ["Xcoord","Ycoord"], 
                  right_on = ["easting","northing"])
 merge_felstiefe_dtm.head()
 merge_felstiefe_dtm.columns
                  
-merge_felstiefe_dtm_felsoberflaeche = pd.merge(merge_felstiefe_dtm, OstrohrR1_Felsoberflaeche_csv, 
+merge_felstiefe_dtm_felsoberflaeche = pd.merge(merge_felstiefe_dtm, OstrohrR1_Felsoberflaeche_csv_sel, 
                  left_on = ["Xcoord","Ycoord"], 
                  right_on = ["easting","northing"])
 merge_felstiefe_dtm_felsoberflaeche.head()
 merge_felstiefe_dtm_felsoberflaeche.columns
 
 
-merge_sel = merge_felstiefe_dtm_felsoberflaeche.loc[:,["Xcoord", "Ycoord", "Zcoord", "tmp14900475028728", "tmp1490047503330", "tmp14900475037932"]]
+merge_sel = merge_felstiefe_dtm_felsoberflaeche.loc[:,["Xcoord", "Ycoord", "Zcoord", "Felstiefe", "DTM", "Felsoberflache"]]
 merge_sel.head()
 
-list(merge_sel)
-merge_sel.columns
-merge_rename1 = merge_sel.rename(columns= {"tmp14900475028728": "Felstiefe"})
-list(merge_rename1)
-merge_rename2 = merge_rename1.rename(columns= {"tmp1490047503330": "DTM"})
-list(merge_rename2)
-merge_rename3 = merge_rename2.rename(columns= {"tmp14900475037932": "Felsoberflache"})
-list(merge_rename3)
+merge_sel['delta_FelsoberflacheZCoord'] = merge_sel.Felsoberflache - merge_sel.Zcoord
+merge_sel.head()
 
-merge_rename3.head()
-merge_rename3.columns
-
-merge_rename3['delta_ZCoordFelstiefe'] = merge_rename3.Zcoord - merge_rename3.Felstiefe
-merge_rename3.head()
+merge_sel.to_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/TunnelVariables.csv", sep="\t")
 
 
-merge_rename3.to_csv("/home/kaelin_joseph/TunnelGIS.Rheintunnel/WORK/Felstiefe.csv", sep="\t")
